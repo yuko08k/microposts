8c9c64b2b56d0cb4710161a275719e9b1e336442
yuuko kataoka <ngnl.ussan@gmail.com>
2017-07-19 05:59:34 +0000

    user login

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show]
+
+  # 一覧ページ
+  def index
+     @users = User.all.page(params[:page])
+  end
+
+  # 個別ページ
+  def show
+    @user = User.find(params[:id])
+  end
+
+  # new と create はセット
+  # new ... 新規登録画面を表示するだけで、ボタンを押すと create に飛ぶ！
+  # create ... 新規登録を実際に行うアクション（だから、create には view が存在しなくて、さっき手動で消した）
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'ユーザを登録しました。'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
+end
